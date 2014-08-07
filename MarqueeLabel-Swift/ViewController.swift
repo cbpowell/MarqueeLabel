540d3dfd20d08f149810beaf1d7e17914f89d240
Charles Powell <cbpowell@gmail.com>
2014-08-06 17:15:57 -0700

    Initial Commit

---
+++ MarqueeLabel-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarqueeLabel-Swift
+//
+//  Created by Charles Powell on 8/6/14.
+//  Copyright (c) 2014 Charles Powell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MarqueeLabel-SwiftTests/MarqueeLabel_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  MarqueeLabel_SwiftTests.swift
+//  MarqueeLabel-SwiftTests
+//
+//  Created by Charles Powell on 8/6/14.
+//  Copyright (c) 2014 Charles Powell. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MarqueeLabel_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
