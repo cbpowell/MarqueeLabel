87cb425bbb79852716f289d5b264ffefbebc3770
Charles Powell <cbpowell@gmail.com>
2015-04-18 09:30:45 -0700

    Add a multi-color, attributed string, continuous-type demo label.

--- MarqueeLabelDemo/Classes/MarqueeLabelDemoViewController.m
+++ MarqueeLabelDemo/Classes/MarqueeLabelDemoViewController.m
@@ -38,17 +38,18 @@
     [super viewDidLoad];
     
     // Continuous Type
+    self.demoLabel1.tag = 101;
     self.demoLabel1.marqueeType = MLContinuous;
     self.demoLabel1.scrollDuration = 15.0;
     self.demoLabel1.animationCurve = UIViewAnimationOptionCurveEaseInOut;
     self.demoLabel1.fadeLength = 10.0f;
     self.demoLabel1.leadingBuffer = 30.0f;
     self.demoLabel1.trailingBuffer = 20.0f;
-    self.demoLabel1.tag = 101;
     // Text string for this label is set via Interface Builder!
     
     
     // Reverse Continuous Type, with attributed string
+    self.demoLabel2.tag = 201;
     self.demoLabel2.marqueeType = MLContinuousReverse;
     self.demoLabel2.scrollDuration = 8.0;
     self.demoLabel2.fadeLength = 15.0f;
@@ -63,6 +64,7 @@
     
     
     // Left/right example, with rate usage
+    self.demoLabel3.tag = 301;
     self.demoLabel3.marqueeType = MLLeftRight;
     self.demoLabel3.rate = 30.0f;
     self.demoLabel3.fadeLength = 10.0f;
@@ -70,17 +72,17 @@
     self.demoLabel3.trailingBuffer = 20.0f;
     self.demoLabel3.textAlignment = NSTextAlignmentCenter;
     self.demoLabel3.text = @"This is another long label that scrolls at a specific rate, rather than scrolling its length in a specific time window!";
-    self.demoLabel3.tag = 102;
      
     
     // Right/left example, with tap to scroll
+    self.demoLabel4.tag = 401;
     self.demoLabel4.marqueeType = MLRightLeft;
     self.demoLabel4.tapToScroll = YES;
     self.demoLabel4.trailingBuffer = 20.0f;
     self.demoLabel4.text = @"This label will not scroll until tapped, and then it performs its scroll cycle only once. Tap me!";
     
-    
     // Continuous, with tap to pause
+    self.demoLabel5.tag = 501;
     self.demoLabel5.marqueeType = MLContinuous;
     self.demoLabel5.scrollDuration = 10.0f;
     self.demoLabel5.fadeLength = 10.0f;
@@ -92,6 +94,20 @@
     tapRecognizer.numberOfTapsRequired = 1;
     tapRecognizer.numberOfTouchesRequired = 1;
     [self.demoLabel5 addGestureRecognizer:tapRecognizer];
+    
+    // Continuous, with attributed text
+    self.demoLabel6.tag = 601;
+    self.demoLabel6.marqueeType = MLContinuous;
+    self.demoLabel6.scrollDuration = 15.0f;
+    self.demoLabel6.fadeLength = 10.0f;
+    self.demoLabel6.trailingBuffer = 30.0f;
+    
+    attributedString = [[NSMutableAttributedString alloc] initWithString:@"This is a long, attributed string, that's set up to loop in a continuous fashion!"];
+    [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:0.123 green:0.331 blue:0.657 alpha:1.000] range:NSMakeRange(0,34)];
+    [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:0.657 green:0.096 blue:0.088 alpha:1.000] range:NSMakeRange(34, attributedString.length - 34)];
+    [attributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f] range:NSMakeRange(0, 16)];
+    [attributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f] range:NSMakeRange(33, attributedString.length - 33)];
+    self.demoLabel6.attributedText = attributedString;
 }
 
 - (IBAction)changeLabelTexts:(id)sender {
@@ -108,6 +124,13 @@
         [attributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f] range:NSMakeRange(19, attributedString.length - 19)];
         self.demoLabel2.attributedText = attributedString;
         
+        attributedString = [[NSMutableAttributedString alloc] initWithString:@"This is a different, longer, attributed string, that's set up to loop in a continuous fashion!"];
+        [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:0.657 green:0.078 blue:0.067 alpha:1.000] range:NSMakeRange(0,attributedString.length)];
+        [attributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f] range:NSMakeRange(0, 16)];
+        [attributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f] range:NSMakeRange(33, attributedString.length - 33)];
+        [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:0.123 green:0.331 blue:0.657 alpha:1.000] range:NSMakeRange(33, attributedString.length - 33)];
+        self.demoLabel6.attributedText = attributedString;
+        
         self.demoLabel1.tag = 102;
     } else {
         self.demoLabel1.text = @"This is a test of MarqueeLabel - the text is long enough that it needs to scroll to see the whole thing.";
@@ -119,6 +142,12 @@
         [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:0.234 green:0.234 blue:0.234 alpha:1.000] range:NSMakeRange(0,attributedString.length)];
         [attributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f] range:NSMakeRange(21, attributedString.length - 21)];
         self.demoLabel2.attributedText = attributedString;
+        
+        attributedString = [[NSMutableAttributedString alloc] initWithString:@"This is a long, attributed string, that's set up to loop in a continuous fashion!"];
+        [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:0.123 green:0.331 blue:0.657 alpha:1.000] range:NSMakeRange(0,attributedString.length)];
+        [attributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f] range:NSMakeRange(0, 16)];
+        [attributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f] range:NSMakeRange(33, attributedString.length - 33)];
+        self.demoLabel6.attributedText = attributedString;
         
         self.demoLabel1.tag = 101;
     }
