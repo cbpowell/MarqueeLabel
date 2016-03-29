f43335653a1483906ff5bc4a272a188f7fa9733c
Charles Powell <cbpowell@gmail.com>
2016-03-28 21:50:24 -0700

    Convert to using a common workspace.

---
+++ MarqueeLabelSwift/MarqueeLabelSwift/MarqueeLabelSwift.h
@@ -0,0 +1,19 @@
+//
+//  MarqueeLabelSwift.h
+//  MarqueeLabelSwift
+//
+//  Created by Charles Powell on 3/27/16.
+//  Copyright © 2016 Charles Powell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MarqueeLabelSwift.
+FOUNDATION_EXPORT double MarqueeLabelSwiftVersionNumber;
+
+//! Project version string for MarqueeLabelSwift.
+FOUNDATION_EXPORT const unsigned char MarqueeLabelSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarqueeLabelSwift/PublicHeader.h>
+
+
