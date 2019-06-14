756385adfff9f26517679e34676d001342d704c0
Charles Powell <cbpowell@gmail.com>
2019-06-14 16:18:02 -0700

    Modernize Package.swift for SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "MarqueeLabel"
+    name: "MarqueeLabel",
+    products: [
+        .library(name: "MarqueeLabel", targets: ["MarqueeLabel"]),
+    ],
+    targets: [
+        .target(
+            name: "MarqueeLabel",
+            path: "Sources"),
+    ]
 )
