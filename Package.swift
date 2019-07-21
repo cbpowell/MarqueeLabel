fcb6d7a899a67a7fe6d8183cf98291d88e6e4c4d
Charles Powell <cbpowell@gmail.com>
2019-07-21 11:01:00 -0700

    Remove macOS as supported platform (for now)

--- Package.swift
+++ Package.swift
@@ -7,8 +7,7 @@ let package = Package(
     name: "MarqueeLabel",
     platforms: [
         .iOS(.v8),
-        .tvOS(.v9),
-        .macOS(.v10_15)
+        .tvOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
