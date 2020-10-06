549c74be174445cdb91cee1deb083fc048b6403f
Charles Powell <cbpowell@gmail.com>
2020-10-05 17:33:05 -0700

    Revert "Remove platform requirement from SPM package file (fixes #255)"
    2c98b1b1c5a53f3821459fd159c5c1f9cf0f8cf5

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "MarqueeLabel",
+    platforms: [
+        .iOS(.v8),
+        .tvOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
