3ed7d273c99181d873d7dcac8faa6864eec18f2c
Aleksandra Generowicz <o.generowicz@gmail.com>
2023-09-28 15:00:32 +0200

    Raise min deployment targets

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "MarqueeLabel",
     platforms: [
-        .iOS(.v9),
-        .tvOS(.v9)
+        .iOS(.v11),
+        .tvOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
