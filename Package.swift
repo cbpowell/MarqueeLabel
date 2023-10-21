992fb3d10e9da6701b14047da862669efc5ba1a8
Charles Powell <cbpowell@gmail.com>
2023-10-21 16:40:29 -0700

    Add PrivacyInfo to Swift package.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MarqueeLabel",
-            path: "Sources")
+            dependencies: [],
+            path: "Sources",
+            resources: [.copy("PrivacyInfo.xcprivacy")])
     ],
     swiftLanguageVersions: [.v5]
 )
