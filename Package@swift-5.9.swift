54df9b980259e5d4379365ea0e5a4955a83132c0
Charles Powell <cbpowell@gmail.com>
2024-02-17 10:17:08 -0700

    Support visionOS in Swift package

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,8 @@ let package = Package(
     name: "MarqueeLabel",
     platforms: [
         .iOS(.v11),
-        .tvOS(.v11)
+        .tvOS(.v11),
+        .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
